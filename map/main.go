d154a9c1d4c2013f0ed028c8211ea4493deb7c6c
suinautant <suinautant@gmail.com>
2021-06-13 14:26:15 +0900

    map

---
+++ map/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	nico := map[string]string{"name": "nico", "age": "12"}
+	// fmt.Println(nico)
+	for key, value := range nico {
+		fmt.Println("KEY : ", key, "/ VALUE : ", value)
+	}
+}

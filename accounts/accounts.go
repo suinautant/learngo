c98efb6e0e931a0d34cf9918a10cb77fa624d3ed
suinautant <suinautant@gmail.com>
2021-06-14 18:42:49 +0900

    accounts app

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,5 +1,13 @@
 package accounts
 
+import (
+	"errors"
+	"fmt"
+)
+
+// var errNoMoney = errors.New("Can't withdraw")
+var errNoMoney = errors.New("Can't withdraw")
+
 // Account struct - nico
 type Account struct {
 	owner   string
@@ -10,4 +18,26 @@ type Account struct {
 func NewAccount(owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
+}
+
+// Deposit x amount on your account
+func (a *Account) Deposit(amount int) {
+	fmt.Println("Gonna deposit : ", amount)
+	a.balance += amount
+}
+
+// Balance of your account
+func (a Account) Balance() int {
+	return a.balance
+}
+
+// Withdraw x amount from your account
+func (a *Account) Withdraw(amount int) error {
+	if a.balance < amount {
+		// return errors.New("Can't withdraw you are poor")
+		return errNoMoney
+	} else {
+		a.balance -= amount
+		return nil
+	}
 }
--- main.go
+++ main.go
@@ -2,11 +2,19 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/suinautant/learngo/accounts"
 )
 
 func main() {
 	account := accounts.NewAccount("nico")
-	fmt.Println(account)
+	account.Deposit(100)
+	fmt.Println(account.Balance())
+	err := account.Withdraw(120)
+	if err != nil {
+		fmt.Println(err)
+		log.Fatalln(err)
+	}
+	fmt.Println(account.Balance())
 }

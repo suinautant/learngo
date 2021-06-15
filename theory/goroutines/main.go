82e81af7d4b0f667d90f163f0b3b0281a2d59e51
suinautant <suinautant@gmail.com>
2021-06-15 20:00:13 +0900

    loop channel

--- theory/goroutines/main.go
+++ theory/goroutines/main.go
@@ -7,20 +7,17 @@ import (
 
 func main() {
 	c := make(chan string)
-	people := [2]string{"nico", "flynn"}
+	people := []string{"nico", "flynn", "yoon", "larry", "pages", "nolan"}
 	for _, person := range people {
 		go isSexy(person, c)
 	}
-	// time.Sleep(time.Second * 5)
-	// result := <-c
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-	fmt.Println(<-c)
-
+	fmt.Println("Waiting for messages")
+	for i := 0; i < len(people); i++ {
+		fmt.Println("Received this message:", <-c)
+	}
 }
 
 func isSexy(person string, c chan string) {
 	time.Sleep(time.Second * 3)
-	fmt.Println(person)
-	c <- person
+	c <- person + " is sexy"
 }

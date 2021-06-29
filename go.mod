184e7ee92524c641a727b23ce3ffebb1ee1a9b95
suinautant <suinautant@gmail.com>
2021-06-29 19:44:56 +0900

    #1.0 getPages part One

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/suinautant/learngo
 
 go 1.16
+
+require github.com/PuerkitoBio/goquery v1.7.0
---
+++ job_scrapper/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
+
+func main() {
+	pages := getPages()
+
+}
+
+func getPages() int {
+	// Request the HTML page.
+	res, err := http.Get(baseURL)
+	checkErr(err)
+	checkCode(res)
+
+	// close at the end of func
+	defer res.Body.Close()
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	doc.Find(".pagination").Each(func(i int, s *goquery.Selection) {
+		fmt.Println(s.Html())
+	})
+
+	return 0
+}
+
+func checkErr(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func checkCode(res *http.Response) {
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+}
